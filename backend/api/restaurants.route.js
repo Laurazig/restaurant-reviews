f344ce22ffc01e81b8fe83ae176c229f1d1cac8a
laurazig <lhzig@protonmail.com>
2022-08-03 20:33:03 +0200

    created new controller post patch delete

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,15 @@
 import express from "express"
 import RestaurantsCtrl from "./restaurants.controller.js"
+import ReviewCtrl from "./reviews.controller.js"
 
 const router = express.Router()
                                         //method
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+
+router
+    .route("/review")
+    .post(ReviewCtrl.apiPostReview)
+    .put(ReviewCtrl.apiUpdateReview)
+    .delete(ReviewCtrl.apiDeleteReview)
 
 export default router
