20c1b18fb651a6eff9acd9d8afdde35c56e56e8e
laurazig <lhzig@protonmail.com>
2022-08-07 19:31:52 +0200

    trying to convert react router dom to v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import AddReview from "./components/add-review";
-import Restaurant from "./components/restaurant";
+import Restaurant from "./components/restaurants";
 import RestaurantsList from "./components/restaurants-list";
 import Login from "./components/login";
 
@@ -20,54 +20,48 @@ function App() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/restaurants"} className="nav-link">
-              Restaurants
-            </Link>
-          </li>
-          <li className="nav-item" >
-            {user ? (
-              <a onClick={logout} className="nav-link" style={{ cursor: 'point' }} >
-                Logout {user.name}
-              </a>
-            ) : (
-              <Link to={"/login"} className="nav-link">
-                Login
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/restaurants" className="navbar-brand">
+            Restaurant Reviews
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/restaurants"} className="nav-link">
+                Restaurants
               </Link>
-            )}
+            </li>
+            <li className="nav-item" >
+              {user ? (
+                <a onClick={logout} className="nav-link" style={{ cursor: 'point' }} >
+                  Logout {user.name}
+                </a>
+              ) : (
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              )}
 
-          </li>
-        </div>
-      </nav>
+            </li>
+          </div>
+        </nav>
 
-      <div className="contailer mt-3">
-        <Switch>
-          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
-          <Route
-            path="restaurants/:id/review"
-            render={(props) => (
-              <AddReview {...props} user={user} />
-            )}
-          />
-          <Route
-            path="/restaurants/:id"
-            render={(props) => (
-              <Restaurant {...props} user={user} />
-            )}
-          />
-          <Route 
+        <div className="contailer mt-3">
+          <Routes>
+            <Route path={["/", "/restaurants"]} element={<RestaurantsList />} />
+            {/* <Route exact path={["/", "/restaurants"]} component={RestaurantsList} /> */}
+            <Route path="restaurants/:id/review" element={<AddReview user={user} />} />
+            <Route path="/restaurants/:id" element={<Restaurant user={user} />} />
+            <Route path="/login" element={<Login login={login} />} />
+            {/* <Route 
             	path="/login"
               render={(props) => (
                 <Login {...props} login={login} />
               )}
-              />
-        </Switch>
-      </div>
+              /> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
