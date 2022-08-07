b2ed4487fffdc4ffbd36cead769311b0d66d0fc6
laurazig <lhzig@protonmail.com>
2022-08-07 12:03:14 +0200

    functions

--- frontend/src/components/restaurants-list.js
+++ frontend/src/components/restaurants-list.js
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/restaurant";
+import { Link } from "react-router-dom";
 
-function RestaurantsList() {
+const RestaurantsList = props => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchZip, setSearchZip] = useState("");
+  const [searchCuisine, setSearchCuisine] = useState("");
+  const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
+  useEffect(() => {
+    retrieveRestaurants();
+    retrieveCuisines();
+  }, []);
+
+  const onChangeSearchName = e => {
+    const searchName = e.target.value;
+    setSearchName(searchName);
+  };
+
+  const onChangeSearchZip = e => {
+    const searchZip = e.target.value;
+    setSearchZip(searchZip);
+  };
+
+  const onChangeSearchCuisine = e => {
+    const searchCuisine = e.target.value;
+    setSearchCuisine(searchCuisine);
+
+  };
+
+  const retrieveRestaurants = () => {
+    RestaurantDataService.getAll()
+      .then(response => {
+        console.log(response.data);
+        setRestaurants(response.data.restaurants);
+
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const retrieveCuisines = () => {
+    RestaurantDataService.getCuisines()
+      .then(response => {
+        console.log(response.data);
+        setCuisines(["All Cuisines"].concat(response.data));
+
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveRestaurants();
+  };
+
+  const find = (query, by) => {
+    
+  }
   return (
     <div className="App">
       hello world
-      
+
     </div>
   );
 }
